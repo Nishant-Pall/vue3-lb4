f3b333bd890cc26b716487b83cb1a25e6ff1a8b2
n-pall <npall@thesummitgrp.com>
2023-10-13 15:38:40 +0530

    setup lint finally

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,47 @@
 <template>
   <NavigationDrawer></NavigationDrawer>
-  <div class="d-flex flex-row">
-    <div class="col">One of three columns</div>
-    <div class="col">One of three columns</div>
-    <div class="col">One of three columns</div>
+  <div>
+    <div class="d-flex flex-row pb-8">
+      <div class="col">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum."
+      </div>
+      <div class="col"></div>
+      <div class="col"></div>
+    </div>
+    <v-divider></v-divider>
+    <div class="d-flex flex-row py-8">
+      <div class="col"></div>
+      <div class="col">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum."
+      </div>
+      <div class="col"></div>
+    </div>
+    <v-divider></v-divider>
+    <div class="d-flex flex-row pt-8">
+      <div class="col"></div>
+      <div class="col"></div>
+      <div class="col">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum."
+      </div>
+    </div>
   </div>
 </template>
 
